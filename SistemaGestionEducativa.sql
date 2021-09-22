fa04e1139b854c675e0ca0e3b18bd9b4826fa9fd
RichLeonC <r29leonc@gmail.com>
2021-09-22 16:52:12 -0600

    Creacion nuevas tablas
    Usuario, UsuarioUbicacion, Padre, Estudiante

--- SistemaGestionEducativa.sql
+++ SistemaGestionEducativa.sql
@@ -8,3 +8,33 @@ create database SistemaGestionEducativa
 go
 use SistemaGestionEducativa
 go
+
+create table Usuario(
+	cedula int not null primary key,
+	nombreCompleto varchar(250) not null,
+	sexo varchar(50),
+	fechaNacimiento date not null,
+
+)
+
+create table Usuario_Ubicacion(
+	cedulaUsuario int not null foreign key references Usuario(cedula),
+	distrito varchar(250) not null,
+	canton varchar(250) not null,
+	localidad varchar(250),
+	provincia varchar(250) not null
+
+)
+
+create table Padre(
+	cedula int not null foreign key references Usuario(cedula),
+	conyugeNombre varchar(250),
+	
+
+)
+
+create table Estudiante(
+	cedula int not null foreign key references Usuario(cedula),
+	cedulaPadre int not null foreign key references Padre(cedula),
+
+)
