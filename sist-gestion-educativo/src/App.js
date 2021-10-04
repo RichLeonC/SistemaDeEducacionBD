991bd8b4f6b0857cef03e118cebf834f3d06d08e
RichLeonC <r29leonc@gmail.com>
2021-10-04 13:46:00 -0600

    Agregacion index.html

--- sist-gestion-educativo/src/App.js
+++ sist-gestion-educativo/src/App.js
@@ -1,7 +1,15 @@
-
+import React,{Component} from 'react';
 import './App.css';
 
-
+class App extends Component{
+    render(){
+        return(
+            <div>
+                Hola
+            </div>
+        )
+    }
+}
 
 export default App;
 
