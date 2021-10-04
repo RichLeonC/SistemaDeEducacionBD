f4a3742ff77c4084a38c539f731a60f2c7deafc1
IssaAC00 <algueracmp@gmail.com>
2021-10-04 15:12:55 -0600

    input

--- sist-gestion-educativo/src/App.js
+++ sist-gestion-educativo/src/App.js
@@ -5,7 +5,24 @@ class App extends Component{
     render(){
         return(
             <div>
-                Hola
+                <h1>
+                Buenas Tardes
+                </h1>
+                <br/>
+                <label>
+                    hello Richi:
+                    <input type= "text"
+                    
+                    />
+
+                </label>
+                <label>
+                    pitudiEdad:
+                    <input type= "text"
+                    
+                    />
+
+                </label>
             </div>
         )
     }
