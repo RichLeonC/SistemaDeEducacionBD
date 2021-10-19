7632a70ef1fb8045d3d4d642f528047f48f87bd1
RichLeonC <r29leonc@gmail.com>
2021-10-19 13:31:12 -0600

    Backend corregido

---
+++ Programa/Backend/API-SGEducativo/API-SGEducativo/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_SGEducativo.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Key]
+        public int cedula { get; set; }
+        public String nombre { get; set; }
+        public String apellido1 { get; set; }
+        public String apellido2 { get; set; }
+        public String contrasenia { get; set; }
+        public String sexo { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public String rol { get; set; }
+        public DateTime fechaCreacion { get; set; }
+    }
+}
