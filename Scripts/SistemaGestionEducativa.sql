f7c0f5fe5531c2712bc30dd904e1feae32f75085
RichLeonC <r29leonc@gmail.com>
2021-10-19 09:46:05 -0600

    Reorganizacion
    se crearon nuevas carpetas para mantener orden

--- Scripts/SistemaGestionEducativa.sql
+++ Scripts/SistemaGestionEducativa.sql
@@ -179,7 +179,7 @@ create table Factura(
 
 )
 
-
+insert into Usuario values(118180009,'Richard','Leon','Chinchilla','0192023a7bbd73250516f069df18b500','Masculino','2001/7/29','admin','2021/10/16')
 
 ---------DROPS DE LAS TABLAS -------------------
 
