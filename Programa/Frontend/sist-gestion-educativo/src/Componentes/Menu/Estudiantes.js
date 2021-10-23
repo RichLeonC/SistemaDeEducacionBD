8cef2503aa5762b3d21fd9c83136a85dbe88d899
RichLeonC <r29leonc@gmail.com>
2021-10-23 11:13:16 -0600

    Update proyecto
    Iniciando con el navbar

--- Programa/Frontend/sist-gestion-educativo/src/Componentes/Menu/Estudiantes.js
+++ Programa/Frontend/sist-gestion-educativo/src/Componentes/Menu/Estudiantes.js
@@ -1,12 +1,14 @@
 import { render } from '@testing-library/react';
 import React,{Component} from 'react';
 import Cookies from 'universal-cookie';
+import SideBar from '../Navegation/Sidebar.js';
 
 function Estudiantes(){
   const cookies = new Cookies();
     return(
         <div>
             <h1>Estudiantes</h1>
+            <SideBar/>
         </div>
     )
     
---
+++ Programa/Frontend/sist-gestion-educativo/src/Componentes/Navegation/SubMenu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SubMenu() {
+    return (
+        <div>
+            SideBar
+        </div>
+    )
+}
