4ba890c030b693bd28da793630ffed83e602152d
IssaAC00 <algueracmp@gmail.com>
2021-11-03 08:19:07 -0600

    asistencia en otra sección

--- Programa/Frontend/sist-gestion-educativo/src/Componentes/PagProfesor/PagAsistencia.js
+++ Programa/Frontend/sist-gestion-educativo/src/Componentes/PagProfesor/PagAsistencia.js
@@ -7,56 +7,59 @@ import { Card,ListGroup,Table} from 'react-bootstrap';
 
 export default function PagAsistencia(props) {
     const cookies = new Cookies();
-    const baseUrl = "https://localhost:44307/api/matriculas";
-    const baseUrlUsuarios =  "https://localhost:44307/api/Estudiante";
-    const [data,setData] = useState([]); //Estado para las matriculas
+    const baseUrlMatriculas = "https://localhost:44307/api/matriculas";
+    const baseUrlUsuarios =  "https://localhost:44307/api/Usuarios";
+    const baseUrlGrupos = "https://localhost:44307/api/Grupos";
+    const [matriculas,setData] = useState([]); //Estado para las matriculas
     const [dataE,setDataE] = useState([]); //Estado para los estudiantes
-    const [grupo, setGrupo]= useState([]);
-
-   const  useCodigoGrupo = (props) =>{
-        setGrupo(this.props.datos)};
+    const [grupodisponibles, setGrupo]= useState([]);
+    const [grupoSeleccionado,setGrupoSeleccionado] = useState([]);
+    var cedula = cookies.get("cedula");// toma la cedula del profesor que haya iniciado sesión. 
+  
         
-    
-    
+
+    const peticionGetG = async()=>{ //Realiza peticiones Get al backend
+        await axios.get(baseUrlGrupos+`/${cedula}`)
+        .then(response=>{
+            setGrupo(response.data);
+        }).catch(error=>{
+            console.log(error);
+        })
+    }
+
    
 
-    const [dataA, setAsistencia]= useState({
-        cedula: '',
-        nombres: '',
-        apellido1:'',
-        apellido2:'',
-        asistencia:''
-
-    });
-
-
-    
     
 
-    const peticionGet = async()=>{ //Realiza peticiones Get al backend Matriculas
-        await axios.get(baseUrl)
+    
+    const peticionGetM = async()=>{ //Realiza peticiones Get al backend estudiantes
+        await axios.get(baseUrlMatriculas)    
         .then(response=>{
             setData(response.data);
         }).catch(error=>{
             console.log(error);
         })
-
     }
 
 
 
-    const peticionGetE = async()=>{ //Realiza peticiones Get al backend Matriculas
+
+
+
+    const peticionGetE = async()=>{ //Realiza peticiones Get al backend estudiantes
         await axios.get(baseUrlUsuarios)    
         .then(response=>{
-            setData(response.dataE);
+            setDataE(response.data);
         }).catch(error=>{
             console.log(error);
         })
     }
 
     useEffect(() => { //Hace efecto la peticion
-        peticionGet();
+       
         peticionGetE();
+        peticionGetM();
+        peticionGetG();
         
     }, [])
 
@@ -66,10 +69,43 @@ export default function PagAsistencia(props) {
     const state = { checked: false }
     const handleCheckboxChange = event => this.setState({ checked: event.target.checked })
  
+    const handlerOpcion = e=>{ //Guarda el grupo selecionado en el estado
+        const opcion = e.target.value;
+        setGrupoSeleccionado(opcion);
+        console.log(opcion);
+     
+        
+    }
 
+    
+     
 
 
     return (// metodo que despliega la tabla que muestra los datos del profesor 
+        <div>
+        <Form>                       
+        <FloatingLabel controlId="floatingSelect" label="Código de grupo">
+            <select id="rol" name="grupos" className="form-control" onChange={handlerOpcion}>
+                <option value ={-1} selected disabled>Opciones</option>
+                 
+            {
+              grupodisponibles.map((item,i)=>(
+                
+                <option key={"grupo"+i} value={item.codigoNombre}>{item.codigoNombre}</option>
+                
+              ))
+            }
+            </select> 
+               
+        </FloatingLabel>
+          
+      </Form>
+
+
+
+
+
+
         <Table className="mt-5 offset-md-0 table table-hover"striped bordered hover variant="light">
             <thead>
                 <tr>
@@ -94,4 +130,6 @@ export default function PagAsistencia(props) {
             
             </tbody>
         </Table>
+    )
+    </div>
     )}
