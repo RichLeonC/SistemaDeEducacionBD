07ac2798d26628dfb620fbff58a55cb4d8f2ffaa
RichLeonC <r29leonc@gmail.com>
2021-11-12 16:45:04 -0600

    Update SbDataAdmin.js

--- Programa/Frontend/sist-gestion-educativo/src/Componentes/Navegation/SbDataAdmin.js
+++ Programa/Frontend/sist-gestion-educativo/src/Componentes/Navegation/SbDataAdmin.js
@@ -2,6 +2,7 @@ import React from 'react'
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
+import * as RiIcons from 'react-icons/ri';
 import * as BiIcons from 'react-icons/bi';
 import * as MdIcons from 'react-icons/md';
 
@@ -10,7 +11,7 @@ export const SbDataAdmin = [
 
     {
         title: 'Estudiante',
-        path: '/Estudiante',
+        path: '/EstadisticaEstudiante',
         icon: <AiIcons.AiFillHome/>, 
         iconClosed: <AiIcons.AiFillCaretDown/>,
         iconOpened :<AiIcons.AiFillCaretUp/>,
@@ -21,6 +22,35 @@ export const SbDataAdmin = [
                 icon: <IoIcons.IoIosPaper/>
             }
         ]
-    }
+    },
+    {
+        title: 'Profesores',
+        path: '/EstadisticaProfesor',
+        icon: <FaIcons.FaChalkboardTeacher/>,
+        iconClosed: <AiIcons.AiFillCaretDown/>,
+        iconOpened :<AiIcons.AiFillCaretUp/>,
+        subNav:[
+            {
+                title: 'TopProfe',
+                path: '/topProfe',
+                icon:<IoIcons.IoIosPaper/>
+            }
+        ]
+    },
+
+    {
+        title: 'Padres',
+        path: '/EstadisticaPadre',
+        icon: <RiIcons.RiParentFill/>,
+        iconClosed: <AiIcons.AiFillCaretDown/>,
+        iconOpened :<AiIcons.AiFillCaretUp/>,
+        subNav:[
+            {
+                title: 'TopPadre',
+                path: '/topPadre',
+                icon:<IoIcons.IoIosPaper/>
+            }
+        ]
+    },
 
 ]
