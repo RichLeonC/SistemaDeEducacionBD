f488525f945886622f0dc48a4d3a56677e3974e8
RichLeonC <r29leonc@gmail.com>
2021-11-18 16:43:59 -0600

    Update

---
+++ Programa/Frontend/sist-gestion-educativo/src/Componentes/Administrador/Estudiantes/PromedioGrupos.js
@@ -0,0 +1,14 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+
+export default function PromedioGrupos() {
+    const baseUrlGrupos = "https://localhost:44329/api/Grupos";
+    
+
+
+    return (
+        <div>
+            <h1>D</h1>
+        </div>
+    )
+}
