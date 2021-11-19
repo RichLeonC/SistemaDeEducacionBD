d1d6e9ab4c93a0af2bf40a37bafd9e3cca33649a
IssaAC00 <algueracmp@gmail.com>
2021-11-19 15:23:35 -0600

    grafico circular

--- Programa/Frontend/sist-gestion-educativo/src/Componentes/Administrador/Estudiantes/CantidadEstGruPe.js
+++ Programa/Frontend/sist-gestion-educativo/src/Componentes/Administrador/Estudiantes/CantidadEstGruPe.js
@@ -5,25 +5,60 @@ import { FloatingLabel } from 'react-bootstrap';
 import axios from 'axios';
 
 export default function CantidadEstGruPe() {
+
+    const baseUrlMatriculas = "https://localhost:44307/api/matriculas";
+
+
+    const [dataMatricula, setDataMatricula]= useState([]);
+     
+    var grupos = dataMatricula.map(es=> es.codigoGrupo);
+    var cantidad= dataMatricula.map(cantidad=> cantidad.cantidadEstudiantes);
+  
+
+
     const data={
-        labels: ['Adrian','Melissa','Richard'],
+        labels: grupos,
         datasets:[{
-            backgroundColor: ['blue', 'yellow', 'red'],
-            data: [89,85,88]
+            backgroundColor: ['#6C63FF', '#5757AF', '#8F8FC3', '#BAB8EF'],
+            data: cantidad
 
         }]
     
     };
 
+
+    const peticionGetMatricula= async()=>{ //Realiza peticiones Get al backend de las matriculas
+        await axios.get(baseUrlMatriculas+"/"+ 2 + "/" + 2021 + "/4")
+        .then(response=>{
+            setDataMatricula(response.data);
+            console.log(dataMatricula);
+        }).catch(error=>{
+            console.log(error);
+        })
+    }
+
+
     const opciones ={
-        responsive: true,
+        presponsive: true,
+        maintainAspectRatio: false
         
     }
 
+    
+
+    useEffect(() => { //Hace efecto la peticion
+        
+        peticionGetMatricula();
+        
+    }, [])
+
+
+
+
     return (
-        <div>
-            <h1> Hola </h1>
-            <Pie  data={data} options={opciones}       />
+        <div style={{width: '90%', height: '500px'}}>
+            
+            <Pie  data={data} options={opciones}/>
         </div>
 
 
