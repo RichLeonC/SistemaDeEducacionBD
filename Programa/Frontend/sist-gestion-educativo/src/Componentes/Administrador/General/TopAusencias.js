d45f7a7d7303e7b29d577499066190d85d75115a
IssaAC00 <algueracmp@gmail.com>
2021-11-21 09:19:52 -0600

    Create TopAusencias.js

---
+++ Programa/Frontend/sist-gestion-educativo/src/Componentes/Administrador/General/TopAusencias.js
@@ -0,0 +1,20 @@
+import React,{useState,useEffect} from 'react';
+import { ModalHeader,Modal,ModalBody,Button,Form,Select,ModalFooter} from 'reactstrap'
+import {Pie , Doughnut} from 'react-chartjs-2';
+import { FloatingLabel } from 'react-bootstrap';
+import axios from 'axios';
+
+export default function TopAusencias(){
+
+    return (
+        <div>
+            <h1>holiwis</h1>
+        </div>
+
+
+
+
+    )
+
+
+}
