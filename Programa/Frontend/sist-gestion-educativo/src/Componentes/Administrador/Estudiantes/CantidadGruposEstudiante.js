dec18c65ca69286b4bd3449e0debd6f72cb70cab
IssaAC00 <algueracmp@gmail.com>
2021-11-21 11:52:08 -0600

    Create CantidadGruposEstudiante.js

---
+++ Programa/Frontend/sist-gestion-educativo/src/Componentes/Administrador/Estudiantes/CantidadGruposEstudiante.js
@@ -0,0 +1,15 @@
+import React,{useState,useEffect} from 'react';
+import { ModalHeader,Modal,ModalBody,Button,Form,Select,ModalFooter} from 'reactstrap';
+import { FloatingLabel } from 'react-bootstrap';
+import axios from 'axios';
+
+export default function CantidadGruposEstudiante(){
+
+    return (
+        <div> 
+            <h1>hola</h1>
+        </div>
+
+    )
+
+}
