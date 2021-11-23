be27c61625daaedafdd07dcdebcb23ba3f7b3320
IssaAC00 <algueracmp@gmail.com>
2021-11-23 11:33:54 -0600

    Create Top15Grupos.js

---
+++ Programa/Frontend/sist-gestion-educativo/src/Componentes/Administrador/Profesores/Top15Grupos.js
@@ -0,0 +1,15 @@
+import React,{useState,useEffect} from 'react';
+import { ModalHeader,Modal,ModalBody,Button,Form,Select,ModalFooter} from 'reactstrap'
+import {Bar} from 'react-chartjs-2';
+import { FloatingLabel } from 'react-bootstrap';
+import axios from 'axios';
+
+
+export default function Top15Grupos(){
+
+    return (
+        <div>hola</div>
+    )
+
+
+}
